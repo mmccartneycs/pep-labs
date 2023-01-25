758d0b2b1fac019b45a9783850fe96e3e48549b0
mmccartneycs <mike_mccartney@msn.com>
2023-01-25 22:06:05 +0000

    Saved progress on OuterJoin 2023-01-25 22:06:05 +0000

---
+++ OuterJoin/problem1.sql
@@ -0,0 +1,1 @@
+Select * from class left outer join student on class.class_title = student.class_title;
---
+++ OuterJoin/problem2.sql
@@ -0,0 +1,1 @@
+Select * from class right outer join textbook on class.class_title = textbook.class_title;

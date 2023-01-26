768f6075da51a87b0b73191581e396414f8670e2
mmccartneycs <mike_mccartney@msn.com>
2023-01-26 20:59:44 +0000

    Saved progress on CombineArrays 2023-01-26 20:59:44 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,11 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] arr = new int[(arr1.length + arr2.length)];
+            System.arraycopy(arr1, 0, arr, 0, arr1.length);
+            System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
 
-        return null;
+
+        return arr;
     }
 }

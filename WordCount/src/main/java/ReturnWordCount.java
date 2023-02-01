0c90b44e95380f7795d087892feee1feb5f1e096
mmccartneycs <mike_mccartney@msn.com>
2023-02-01 23:22:08 +0000

    Saved progress on WordCount 2023-02-01 23:22:08 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,7 +5,7 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        int inc = 2;
+        int inc = 1;
         String temp = in;
         boolean finished = false;
         while(finished == false){

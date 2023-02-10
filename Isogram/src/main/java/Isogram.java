c9665803c55505604ba1164033de4c66c5d07d0c
mmccartneycs <mike_mccartney@msn.com>
2023-02-10 12:55:00 +0000

    Saved progress on Isogram 2023-02-10 12:55:00 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Isogram {
     /**
@@ -13,6 +14,16 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        boolean iso = false;
+        for(int i = 0; i<str.length();i++){
+            if(i == str.lastIndexOf(str.charAt(i))){
+                iso = true;
+            }
+            if(i != str.lastIndexOf(str.charAt(i))){
+                iso = false;
+                break;
+            }
+        }
+        return iso;
     }
 }

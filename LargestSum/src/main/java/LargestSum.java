91dbe1bf621aaf7dee30d37af1f4da5f9008fe60
mmccartneycs <mike_mccartney@msn.com>
2023-02-12 11:49:56 +0000

    Saved progress on LargestSum 2023-02-12 11:49:56 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -10,6 +10,15 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int bigMax = 0;
+        int littleMax = 0;
+        for( int i = 0; i < nums.size(); i++){
+            if(nums.get(i) >= bigMax){
+                littleMax = bigMax; 
+                bigMax = nums.get(i);
+
+            }
+        }
+        return littleMax + bigMax;
     }
 }
